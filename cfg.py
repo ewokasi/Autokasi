a0e29f773952f302dcb512bb1e25d7304554d70b
ewokasi <kostyakov0203@mail.ru>
2021-04-18 00:40:04 +1200

    All fixed boiiiiiiii

--- cfg.py
+++ cfg.py
@@ -8,13 +8,20 @@ from datetime import datetime
 
 TOKEN = 'NjcxNjc3OTUxMDc0MzY5NTQw.XjS6vg.XqCZS0o5EqzjmQrs_SjbwUFeUPk'
 bot = commands.Bot(command_prefix='%')
-print("im ready")
+
+html_Tablica_tarkovhelp = requests.get('https://escapefromtarkov.fandom.com/ru/wiki/%D0%91%D0%BE%D0%B5%D0%BF%D1%80%D0%B8%D0%BF%D0%B0%D1%81%D1%8B')
+soup = BeautifulSoup(html_Tablica_tarkovhelp.text, "html.parser")
+
+
+
+
 
 @bot.command(pass_context=True)
 async def п(ctx, arg):
-
+    dobav=0
     c=0
-
+    cou=0
+    bilo=[]
     author = ctx.author
     patr = ""
     patr = patr+arg
@@ -28,55 +35,63 @@ async def п(ctx, arg):
     vers.append(red2)
     vers.append(red3)
 
-    if "бп" in vers:
-        c=3
 
-
-
-    html_Tablica_tarkovhelp = requests.get(
-        'https://escapefromtarkov.fandom.com/ru/wiki/%D0%91%D0%BE%D0%B5%D0%BF%D1%80%D0%B8%D0%BF%D0%B0%D1%81%D1%8B')
-    soup = BeautifulSoup(html_Tablica_tarkovhelp.text, "html.parser")
     flag = 0
     for i in vers :
-        print(len(set(soup.findAll('a',text=re.compile(i)))))
+        b=len(set(soup.findAll('a',text=re.compile(i))))
 
-        if len(set(soup.findAll(text=re.compile(i))))>0 and c<=len(set(soup.findAll(text=re.compile(i)))) and flag ==0:
+        print(set(soup.findAll('a', text=re.compile(i))))
 
+
+        if len(set(soup.findAll(text=re.compile(i))))>0 and c<=b and flag ==0:
             tableAll = soup.findAll(text=re.compile(i))
 
 
             for j in tableAll:
-                table = j.find_parent('tr')
                 c = c + 1
-                output = table.find("img")
-                icon = output.get("src")
-                output = table.findChild("td")
-                name = output.text
-                output = table.findAll("td")
-                dmg = str(output[1])
-                dmg = dmg[4:len(dmg) - 5]
+                try:
+                    table = j.find_parent('tr')
 
-                pen = str(output[2])
-                pen = pen[4:len(pen) - 5]
-                armordmg = str(output[3])
-                armordmg = armordmg[4:len(armordmg) - 5]
-                accu = str(output[4])
-                accu = accu[25:len(accu) - 12]
-                recoil = str(output[5])
-                recoil = recoil[25:len(recoil) - 12]
-                fragm = str(output[6])
-                fragm = fragm[4:len(fragm) - 5]
-                bounce = str(output[7])
-                bounce = bounce[4:len(bounce) - 5]
-                speed = str(output[8])
-                speed = speed[4:len(speed) - 5]
+                    output = table.find("img")
+                    icon = output.get("src")
+                    output = table.findChild("td")
+                    name = output.text
+                    output = table.findAll("td")
+                    dmg = str(output[1])
+                    dmg = dmg[4:len(dmg) - 5]
 
-                await ctx.send(f" {icon}")
-                await ctx.send(f"```{name}, \nУрон:{dmg}, Пробитие: {pen}, Урон броне: {armordmg}%, Скорость: {speed}```")
-                flag=1
-                print(c)
-                if c == len(set(soup.findAll(text=re.compile(i)))):
-                    break
+
+                    pen = str(output[2])
+                    pen = pen[4:len(pen) - 5]
+                    armordmg = str(output[3])
+                    armordmg = armordmg[4:len(armordmg) - 5]
+                    accu = str(output[4])
+                    accu = accu[25:len(accu) - 12]
+                    recoil = str(output[5])
+                    recoil = recoil[25:len(recoil) - 12]
+                    fragm = str(output[6])
+                    fragm = fragm[4:len(fragm) - 5]
+                    bounce = str(output[7])
+                    bounce = bounce[4:len(bounce) - 5]
+                    speed = str(output[8])
+
+                    speed = speed[4:len(speed) - 5]
+                    if name not in bilo:
+
+                        bilo.append(name)
+                    else: continue
+
+                    if "шт" in table.extract():
+                        continue
+                    await ctx.send(f" {icon}")
+                    await ctx.send(f"```{name}, \nУрон:{dmg}, Пробитие: {pen}, Урон броне: {armordmg}%, Скорость: {speed}```")
+                    flag=1
+                    print(c)
+                    if c == len(set(soup.findAll(text=re.compile(i)))):
+                        break
+                except:
+
+                    continue
 
         if flag==1:
             break
@@ -124,6 +139,5 @@ async def status(ctx, user: discord.Member):
 
     fulldate = str(user.name+' joined ' + str(date.day) + "'th of " + month + " " + str(date.year))
     await ctx.send(fulldate)
-
+print("im ready")
 bot.run(TOKEN)
-
